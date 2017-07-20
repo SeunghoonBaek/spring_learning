de270e66e00c8000fca162414a40baa1add1e368
LinePlus <lineplus@AD01248016.local>
2017-07-20 20:06:43 +0900

    update web

---
+++ spring_web/src/main/java/idv/bsh/web/StudentController.java
@@ -0,0 +1,36 @@
+package idv.bsh.web;
+
+import jdk.nashorn.internal.ir.RuntimeNode;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by lineplus on 2017. 7. 19..
+ */
+
+@Controller
+public class StudentController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String welcome(){
+        return "redirect:/student";
+    }
+
+    @RequestMapping(value = "/student", method = RequestMethod.GET)
+    public ModelAndView student() {
+        return new ModelAndView("student", "command", new Student());
+    }
+
+
+    @RequestMapping(value = "/addStudent", method = RequestMethod.POST)
+    public String addStudent(Student student, ModelMap model) {
+        model.addAttribute("name", student.getName());
+        model.addAttribute("age", student.getAge());
+        model.addAttribute("id", student.getId());
+
+        return "result";
+    }
+}
---
+++ spring_web/web/WEB-INF/jsp/result.jsp
@@ -0,0 +1,33 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: lineplus
+  Date: 2017. 7. 19.
+  Time: PM 6:53
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Spring MVC Form Handling</title>
+</head>
+<body>
+    <h2>Submitted Student Information</h2>
+
+    <table>
+        <tr>
+            <td>name</td>
+            <td>${name}</td>
+        </tr>
+
+        <tr>
+            <td>age</td>
+            <td>${age}</td>
+        </tr>
+
+        <tr>
+            <td>id</td>
+            <td>${id}</td>
+        </tr>
+    </table>
+</body>
+</html>
